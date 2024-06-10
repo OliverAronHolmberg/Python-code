4d00c00f5203c257475a4f75908be842f859ec15
Oliver Holmberg <63006762+Oliver1526@users.noreply.github.com>
2024-06-10 18:03:27 +0200

    Changed every string to english

--- Övningar/spel/stensaxpåse.py
+++ Övningar/spel/stensaxpåse.py
@@ -11,37 +11,37 @@ class Game:
     def Gissning(self, gissat):
         self.gissat = gissat
         if gissat == "rock" and self.val == "scissors":
-            print("I chose scissors")
+            print("I chose Scissors")
             print("You Won")
         elif gissat == "scissors" and self.val == "paper":
-            print("Jag valde påse")
-            print("Du vann")
+            print("I chose Paper")
+            print("You Won")
         elif gissat == "paper" and self.val == "rock":
-            print("Jag valde sten")
-            print("Du vann")
+            print("I chose Rock")
+            print("You Won")
         
         elif gissat == "scissors" and self.val == "rock":
-            print("Jag valde sten")
-            print("Jag vann")
+            print("I chose Rock")
+            print("I won!")
         elif gissat == "paper" and self.val == "scissors":
-            print("Jag valde sax")
-            print("Jag vann")
+            print("I chose Scissors")
+            print("I won!")
         elif gissat == "rock" and self.val == "paper":
-            print("Jag valde påse")
-            print("Jag vann")
+            print("I chose Paper")
+            print("I won!")
         else:
-            print(f"Jag valde {self.val}")
-            print("oavgjort")
+            print(f"I chose {self.val}")
+            print("It was a draw")
         
         
-        while self.spela_igen == "ja" or "nej":
-            self.spela_igen = input("Vill du spela Ja/Nej").lower()
-            if self.spela_igen == "ja":
-                print("Vad kul")
+        while self.spela_igen == "yes" or "no":
+            self.spela_igen = input("Do you want to play more?").lower()
+            if self.spela_igen == "yes":
+                print("How fun")
                 print("")
                 break
-            elif self.spela_igen == "nej":
-                print("Hejdå")
+            elif self.spela_igen == "no":
+                print("See you next time!")
                 game.play == False
                 quit()
             else:
@@ -51,16 +51,16 @@ class Game:
 
 game = Game()
 
-print("!!Välkommen till stex/sax/påse!!")
+print("!!Welcome to Rock/Paper/Scissors!!")
 
 while game.play == True:
     game.val = random.choice(game.lista)
-    gissning = input("Skriv: Sten/Sax/Påse").lower()
+    gissning = input("Please write either: Rock/Paper/Scissors").lower()
     
     
-    if gissning == "sten" or gissning == "sax" or gissning == "påse":
+    if gissning == "Rock" or gissning == "Scissors" or gissning == "Paper":
         gis = game.Gissning(gissning)
     
     else:
-        print("Du kan inte fuska din fuskis!")
+        print("You cannot cheat you cheater")
 
