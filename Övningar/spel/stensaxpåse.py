3baad3242f253dbe072c22fb70eeb2b3f7a51495
Oliver Holmberg <63006762+Oliver1526@users.noreply.github.com>
2024-06-10 18:18:46 +0200

    Made the program look nicer

--- Övningar/spel/stensaxpåse.py
+++ Övningar/spel/stensaxpåse.py
@@ -10,6 +10,9 @@ class Game:
 
     def Gissning(self, gissat):
         self.gissat = gissat
+
+        print("")
+
         if gissat == "rock" and self.val == "scissors":
             print("I chose Scissors")
             print("You Won")
@@ -33,12 +36,13 @@ class Game:
             print(f"I chose {self.val.title()}")
             print("It was a draw")
         
-        
+        print("")
         while self.spela_igen == "yes" or "no":
             self.spela_igen = input("Do you want to play more? Yes/No").lower()
             if self.spela_igen == "yes":
                 print("How fun")
                 print("")
+                print("**********************************")
                 break
             elif self.spela_igen == "no":
                 print("See you next time!")
@@ -50,9 +54,9 @@ class Game:
 
 
 game = Game()
-
+print("**********************************")
 print("!!Welcome to Rock/Paper/Scissors!!")
-
+print("**********************************")
 while game.play == True:
     game.val = random.choice(game.lista)
     gissning = input("Please write either: Rock/Paper/Scissors").lower()
@@ -63,4 +67,5 @@ while game.play == True:
     
     else:
         print("You cannot cheat you cheater")
+        print("")
 
