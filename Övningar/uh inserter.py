ba5de9f4dc5305390242a97bfe6b90090bdb16f6
Oliver Holmberg <63006762+Oliver1526@users.noreply.github.com>
2024-06-11 09:42:58 +0200

    Last rock paper scissors commit

---
+++ Övningar/uh inserter.py
@@ -0,0 +1,48 @@
+import random
+
+class Main:
+
+
+    def __init__(self):
+        self.run = True
+        
+
+    def AddUh(self, sentance):
+        self,sentance = sentance
+
+
+
+
+
+main = Main()
+
+
+while main.run == True:
+    type = input("How do you need the sentance (lowercase/uppercase/capitalize/title): ").lower()
+    if type == "lowercase":
+        sentance = input("Write your sentance (Quit to quit)").lower()
+        main.AddUh(sentance)
+    elif type == "uppercase":
+        sentance = input("Write your sentance (Quit to quit)").upper()
+        main.AddUh(sentance)
+    elif type == "capitalize":
+        sentance = input("Write your sentance (Quit to quit)").capitalize()
+        main.AddUh(sentance)
+    elif type == "title":
+        sentance = input("Write your sentance (Quit to quit)").title()
+        main.AddUh(sentance)
+    else:
+        print("Plase awswer correctly")
+        print("")
+        continue
+        
+        
+    
+    
+    
+
+    
+    
+
+
+    
